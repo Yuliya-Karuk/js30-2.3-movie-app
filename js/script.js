95243d5e5e7dbf5706727a99fcf3ea6c7a0d35e2
Yuliya-Karuk <karuk.yuliya@gmail.com>
2023-06-20 19:12:39 +0400

    fix: change some CSS for section Top film, add some links

--- js/script.js
+++ js/script.js
@@ -5,13 +5,20 @@ const buttonClear = document.querySelector('.clear-btn'); // кнопка очи
 const popularList = document.querySelector('.popular-list'); // список с популярными фильмами
 const searchList = document.querySelector('.search-list'); // список с результатами поиска
 const topFilm = document.querySelector('.top-film'); // список с результатами поиска
+const searchTitle = document.querySelector('.search-results>.section-title'); // список с результатами поиска
 let buttonHeart = []
-const apiKey = '16d35851d48e8bf86f00899a21ddb357'
-const baseUrl = "https://api.themoviedb.org/3/"
-const language = "en-US"
-const page = 1
-const popularUrl = `${baseUrl}movie/popular?api_key=${apiKey}&language=${language}&page=${page}`;
+const popularUrl = `https://api.themoviedb.org/3/movie/popular?api_key=16d35851d48e8bf86f00899a21ddb357&language=en-US&page=1`;
+const genresUrl = 'https://api.themoviedb.org/3/genre/movie/list?api_key=16d35851d48e8bf86f00899a21ddb357&language=en';
 
+//функция, которая возвращает жанры фильма
+function getGenre(neededArr, arr) {
+    return x = arr.reduce(((acc, el) => {
+        if (neededArr.includes(el.id)) {
+            acc = [...acc, el.name]
+        }
+        return acc
+    }), [])
+}
 
 // функция, которая после загрузки страницы ставит фокус на инпут
 function inputFocus() {
@@ -24,21 +31,25 @@ function clearInput() {
     buttonClear.blur()
     searchList.innerHTML = ''
     searchList.classList.add('visually-hidden')
+    searchTitle.classList.add('visually-hidden')
 }
 
 // функция, которая получает значение, введенное в инпут и выдает по нему фильмы
 function getInput(event) {
     event.preventDefault();
-    let searchUrl = `${baseUrl}search/movie?query=${searchInput.value}&api_key=${apiKey}&language=${language}&page=${page}`;
+    let searchUrl = `https://api.themoviedb.org/3/search/movie?query=${searchInput.value}&api_key=16d35851d48e8bf86f00899a21ddb357&language=en-US&page=1`;
     getMovies(searchUrl, searchList);
     searchList.classList.remove('visually-hidden')
+    searchTitle.classList.remove('visually-hidden')
 }
 
+// функция, которая перекрашивает сердцев красный
 function colorButtonHeart(event) {
     event.preventDefault();
     event.target.classList.add("movie-favorite-red");
 }
 
+// функция, которая выводит список фильмов
 async function getMovies(url, htmlEl) {
     const res = await fetch(url);
     const data = await res.json();
@@ -46,14 +57,16 @@ async function getMovies(url, htmlEl) {
     data.results.forEach((el) => {
         htmlEl.insertAdjacentHTML("beforeend",
             `<li class="movie-item">
-                <img class="movie-poster" src="https://www.themoviedb.org/t/p/w1280/${el.poster_path}" width="200" height="100%" >
-                <div class="movie-overview visually-hidden">${el.overview}</div>
+                <a class="movie-wrapper-link" href="#" aria-label="link on movie">
+                    <img class="movie-poster" src="https://www.themoviedb.org/t/p/w1280/${el.poster_path}" width="200" height="100%" alt="movie poster">
+                    <p class="movie-overview">${el.overview}</p>
+                </a>
                 <div class="movie-details">
-                    <h4 class="movie-title">${el.title}</h4>
+                    <a href="" class="movie-title">${el.title}</a>
                     <p class="movie-date">${el.release_date}</p>
                     <div class="movie-description">
                         <div class="movie-rate">
-                            <img class="imbd-img" src="images/imbd.svg" width="28" height="14" >
+                            <img class="imbd-img" src="images/imbd.svg" width="28" height="14" alt="icon IMBD rating">
                             <p class="imbd-rate">${el.vote_average}</p>
                         </div>
                         <button class="movie-favorite" type="button" aria-label="click to add to favorite"></button>
@@ -67,24 +80,38 @@ async function getMovies(url, htmlEl) {
     buttonHeart.forEach((el) => el.addEventListener("click", colorButtonHeart))
 }
 
+// функция, которая выводит 1 топовый фильм на переднем плане
 async function showTopMovie(url) {
     const res = await fetch(url);
     const data = await res.json();
     const neededFilm = data.results[2]
 
+    const resGenres = await fetch(genresUrl);
+    const dataGenres = await resGenres.json();
+    const neededGenres = getGenre(neededFilm.genre_ids, dataGenres.genres).join(', ')
+
     topFilm.insertAdjacentHTML("beforeend",
-        //`<img class="movie-poster" src="https://www.themoviedb.org/t/p/w1280/${neededFilm.poster_path}" width="1280" height="500px" >
         `<div class="movie-top-poster"></div>
         <div class="movie-top-description">  
-            <h2 class="movie-title">${neededFilm.title}</h2>
-            <p class="movie-overview">${neededFilm.overview}</p>
-            <p class="movie-overview">${neededFilm.genre_ids}</p>
-            <div class="movie-details">
-                <a class="watch" type="button" aria-label="link to watch film"></a>
-                <button class="add-favorite" type="button" aria-label="click to add to favorite"></button>
+            <a href="#" class="movie-title">${neededFilm.title}</a>
+            <p class="movie-top-overview">${neededFilm.overview}</p>
+            <p class="movie-genres">${neededGenres}</p>
+            <div class="top-movie-details">
+                <a class="watch" type="button" aria-label="link to watch film">
+                    <span>Watch</span>
+                    <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M2 2L14 8.6394L2 15.2788V2Z" fill="white" stroke="white" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                </a>
+                <button class="add-favorite" type="button" aria-label="click to add to favorite">
+                    <span>My list</span>
+                    <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M16.936 10.2H10.564V16.5H6.712V10.2H0.34V6.6H6.712V0.299998H10.564V6.6H16.936V10.2Z" fill="white"/>
+                    </svg>
+                </button>
             </div>
             <div class="movie-rate">
-                <img class="imbd-img" src="images/imbd.svg" width="28" height="14" >
+                <img class="imbd-img" src="images/imbd.svg" width="28" height="14" alt="icon IMBD rating">
                 <p class="imbd-rate">${neededFilm.vote_average}</p>
                 <p class="movie-date">${neededFilm.release_date}</p>
             </div>
@@ -94,22 +121,10 @@ async function showTopMovie(url) {
     document.querySelector('.movie-top-poster').style.backgroundImage = `url(https://www.themoviedb.org/t/p/w1280/${neededFilm.poster_path})`
 }
 
-getMovies(popularUrl, popularList); //загрузить стартовую старницу с популярными фильмами
-showTopMovie(popularUrl)
+getMovies(popularUrl, popularList); //загрузить стартовую страницу с популярными фильмами
+showTopMovie(popularUrl) //загрузить секцию с 1 топовым фильмом
 
 // Listeners
 document.addEventListener("DOMContentLoaded", inputFocus); // фокус на инпуте после загрузки страницы
 searchForm.addEventListener("submit", getInput); // получить значение инпута
 buttonClear.addEventListener("click", clearInput); // очистить инпут при нажатии на крестик
-
-// const apiKey = '95d48fcf'
-// const startedUrl = `http://www.omdbapi.com/?apikey=95d48fcf&`
-
-// async function getData(url) {
-//   const res = await fetch(url);
-//   const data = await res.json();
-//   console.log(data);
-// }
-// getData(url);
-
-
